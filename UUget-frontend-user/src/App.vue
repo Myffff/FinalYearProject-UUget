b2ea4eaddac1197364d33a90c49a1bc0405aeab5
Yifei Ma <634042441@qq.com>
2022-12-02 06:07:27 +0800

    init project. Add basic code of login page. no element implemented.

---
+++ UUget-frontend-user/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Login from "./components/Login.vue";
+</script>
+
+<template>
+  <div id="app">
+    <header></header>
+
+    <main>
+      <div>
+        <Login />
+      </div>
+    </main>
+  </div>
+</template>
